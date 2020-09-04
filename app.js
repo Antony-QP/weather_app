e5b91c4d132cd300abd346e9c07908817a65c395
Antony Parker <antonyqparker@gmail.com>
2020-09-04 15:01:20 +0100

    very basic UX.  Displaying weatherdata successfully

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Initialize weather object
 const weather = new Weather('london');
-
+// Init ui
+const ui = new UI()
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
 function getWeather() {
   weather.getWeather()
     .then(results => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch(err => console.log(err));
 }
---
+++ ui.js
@@ -0,0 +1,18 @@
+class UI {
+  constructor() {
+    this.location = document.getElementById('location');
+    this.icon = document.getElementById('icon');
+    this.temp = document.getElementById('temperature')
+    this.description = document.getElementById('weather-info');
+    this.feelsLike = document.getElementById('feels-like');
+  }
+
+  paint(weather) {
+
+
+    this.location.textContent = weather.name
+    this.temp.textContent = JSON.stringify(weather.main.temp) + " Degrees Celcius"
+    this.description.textContent = JSON.stringify(weather.weather[0].description)
+    this.feelsLike.textContent = weather.main.feels_like + " Degrees Celcius"
+  }
+}
